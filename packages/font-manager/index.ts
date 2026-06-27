2fd56b7fdf047934d442fa4de0b5dc04b0cb32d9
Osei Fortune <fortune.osei@yahoo.com>
2026-06-26 20:18:07 -0400

    fix: typings

--- packages/font-manager/index.ts
+++ packages/font-manager/index.ts
@@ -1,2 +1,4 @@
-export * from './font-face';
-export * from './font-face-set';
+export { FontFace, importFontsFromCSS, loadFontsFromCSS } from './font-face';
+export { FontFaceSet } from './font-face-set';
+
+export type { stretch, stretchName, stretchPercent } from './font-face';
